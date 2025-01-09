819a03acc36f905a97986b0704aa3795c268107b
tonybnya <nya.tony2010@gmail.com>
2025-01-09 12:46:55 +0100

    style: add a custom icon for the projects in the menu

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,11 @@
 import { Admin, Resource } from "react-admin";
+import CategoryIcon from "@mui/icons-material/Category";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
 import ProjectsList from "./pages/projects/projectsList";
 
 export const App = () => (
   <Admin layout={Layout} dataProvider={dataProvider}>
-    <Resource name="projects" list={ProjectsList} />
+    <Resource name="projects" list={ProjectsList} icon={CategoryIcon} />
   </Admin>
 );
