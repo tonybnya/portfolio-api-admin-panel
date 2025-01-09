965f026e336bcf50dacc84cce5adf8838ef01312
tonybnya <nya.tony2010@gmail.com>
2025-01-09 15:45:56 +0100

    chore: add Dashboard.tsx and update App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,17 +1,20 @@
 import { Admin, Resource } from "react-admin";
-import CategoryIcon from "@mui/icons-material/Category";
+import TerminalIcon from "@mui/icons-material/Terminal";
+import TimelineIcon from "@mui/icons-material/Timeline";
 import { Layout } from "./Layout";
 import { dataProvider } from "./dataProvider";
+import Dashboard from "./Dashboard";
 import ProjectsList from "./pages/projects/projectsList";
 import ProjectShow from "./pages/projects/projectShow";
 
 export const App = () => (
-  <Admin layout={Layout} dataProvider={dataProvider}>
+  <Admin layout={Layout} dataProvider={dataProvider} dashboard={Dashboard}>
     <Resource
       name="projects"
       list={ProjectsList}
       show={ProjectShow}
-      icon={CategoryIcon}
+      icon={TerminalIcon}
     />
+    <Resource name="timelines" icon={TimelineIcon} />
   </Admin>
 );
---
+++ src/Dashboard.tsx
@@ -0,0 +1,10 @@
+import { Card, CardContent, CardHeader } from "@mui/material";
+
+const Dashboard = () => (
+  <Card>
+    <CardHeader title="Welcome to My Portfolio API Administration Panel" />
+    <CardContent>Lorem ipsum sic dolor amet...</CardContent>
+  </Card>
+);
+
+export default Dashboard;
